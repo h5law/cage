6bc522484a3a032ab8c5709865934834de94b3a7
h5law <dev@h5law.com>
2026-09-18 21:36:45 +0100

    container: isolate environment variables from host to child setting them explicitly

--- src/container.c
+++ src/container.c
@@ -914,6 +914,22 @@ static int run_command(struct child_context *ctx)
         if (unblock_forwarded_signals() == -1)
             _exit(127);
 
+        /*
+         * Set the env variables for the child process to stop
+         * the container inheriting the host env
+         */
+        char *term = getenv("TERM");
+        clearenv();
+        setenv("PATH",
+               "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+               1);
+        setenv("HOME", "/root", 1);
+        setenv("USER", "root", 1);
+        setenv("LOGNAME", "root", 1);
+        setenv("LANG", "C.UTF-8", 1);
+        if (term != NULL)
+            setenv("TERM", term, 1);
+
         execvp(ctx->container->argv[0], ctx->container->argv);
 
         perror("exec");
